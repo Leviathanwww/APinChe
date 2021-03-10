071f2dca015dc6a9cb2d56cb583f9655c4c5cb5e
fcy <2275503952@qq.com>
2021-03-10 21:01:35 +0800

    更新了大厅页面

--- 校园拼车/pages/lobby-page/lobby-page.js
+++ 校园拼车/pages/lobby-page/lobby-page.js
@@ -5,7 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    location:"阳澄湖",
+    room:[{
+        id:0,
+        label:"快上车",
+        nickName:"一路向北",
+        owner:"方子焱方子方子焱",
+        departureTime:"00:00",
+        destination:"天虹",
+        number: 2,
+      },
+      {
+        id: 42443242,
+        nickName:"一路向北向向向123",
+        label:"快上车车打车打车打车打车打",
+        owner:"方子焱",
+        departureTime:"00:00",
+        destination:"本部",
+        number: 1,
+      }
+    ]
   },
 
   /**
